93bc9e0502abe6f3b999dcfbce7d1768266b4cc1
unknown <baklazhkov@bk.ru>
2020-11-17 13:17:04 +0300

    task23

--- Practice/23/c++/Project1/Project1/Source.cpp
+++ Practice/23/c++/Project1/Project1/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <iomanip>
 #include"Header2.h"
 #include"Header1.h"
-#define M_PI
 int main() {
 	std::cout << "n		n!\n";
 	for (int i = 1; i <= 10; i++) {
@@ -9,11 +9,12 @@ int main() {
 	}
 	std::cout << "\n";
 	std::cout << "x		sin(x)\n";
-/*	double x = 0;
+	double x = 0, Pi = 3.1415926535;
+
 	for (int i = 0; i <= 46; i++) {
-		std::cout << x << "		" << sin2(x) << "\n";
-		x = x + (M_PI \ 180);
-	}*/
+		std::cout << std::setprecision(4) << x << "		" << sin2(x) << "\n";
+		x = x + (Pi / 180);
+	}
 	std::cout << "\nk		C(k, 10)\n";
 	for (int i = 1; i <= 10; i++) {
 		std::cout << i << "		" << cox(i) << "\n";
